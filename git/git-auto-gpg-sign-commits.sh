399d1f2e9fcfad937c54a144d027977cfe140771
EddieED <30291338+EddieWongED@users.noreply.github.com>
2023-12-21 18:25:48 +0800

    feat: Add more description to git auto gpg signing

--- git/git-auto-gpg-sign-commits.sh
+++ git/git-auto-gpg-sign-commits.sh
@@ -9,6 +9,11 @@ gpg --list-secret-keys --keyid-format LONG
 # For example: rsa2048/B2D2BE026E4EE25C -> B2D2BE026E4EE25C is the key id
 gpg --armor --export [YOUR_KEY_ID]
 
+# Go to https://github.com/settings/keys. Click "New GPG key"
+# Copy the public key and save.
+# Copy from -----BEGIN PGP PUBLIC KEY BLOCK-----
+# To -----END PGP PUBLIC KEY BLOCK-----
+
 # Set a gpg key for git
 git config --global user.signingkey [YOUR_KEY_ID]
 
@@ -17,3 +22,5 @@ git config --global commit.gpgsign true
 
 # Or sign only a specific commit
 git commit -S -m "This is an signed commit"
+
+# After you push the commit, you should see the commit is verified
